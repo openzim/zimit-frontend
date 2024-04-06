ea66868f0010c4ef167e06036474adafc82c715b
Onyx2406 <yashsancheti24@gmail.com>
2024-04-06 23:56:07 +0530

    Fix footer text

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -4,12 +4,13 @@
     <AlertFeedback />
     <router-view />
     <footer>
-    {{ $t('app.poweredByKiwix') }} <a target="_blank" :href="$t('app.kiwixLink')">{{ $t('app.kiwix') }}</a>
-    {{ $t('app.and') }} 
-    <a target="_blank" :href="$t('app.webrecorderLink')">{{ $t('app.webrecorder') }}</a>, 
-    {{ $t('app.thanksToMozilla') }} <a target="_blank" href="https://www.mozilla.org/moss/">Mozilla Open-Source Support Award</a> 
-    <b-icon icon="heart-fill" style="color: rgb(234, 74, 170);" />
-  </footer>
+      {{ $t('app.poweredBy') }} 
+      <a target="_blank" :href="$t('app.kiwixLink')">{{ $t('app.kiwix') }}</a>
+      {{ $t('app.and') }} 
+      <a target="_blank" :href="$t('app.webrecorderLink')">{{ $t('app.webrecorder') }}</a>, 
+      <span v-html="$t('app.thanksToMozilla')"></span>
+      <b-icon icon="heart-fill" style="color: rgb(234, 74, 170);" />
+    </footer>
   </div>
 </template>
 
@@ -35,5 +36,16 @@ footer {
   text-align: center;
   padding-top: 1em;
   padding-bottom: 1em;
+}
+
+/* RTL styles */
+[dir="rtl"] #app {
+  direction: rtl;
+  text-align: right;
+}
+
+[dir="rtl"] footer {
+  direction: rtl;
+  text-align: center; /* Keep footer text centered or change as needed */
 }
 </style>
