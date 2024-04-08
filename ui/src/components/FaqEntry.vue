ac1bb17488c4001e12bcf104af2c3b661bc11285
Onyx2406 <yashsancheti24@gmail.com>
2024-04-09 00:32:25 +0530

    RTL Support for buttons of the FAQ

--- ui/src/components/FaqEntry.vue
+++ ui/src/components/FaqEntry.vue
@@ -1,41 +1,54 @@
 <template>
-    <b-card no-body class="mb-1">
-      <b-card-header header-tag="header" class="p-1" role="tab">
-        <b-button block v-b-toggle="[id]" variant="neutral">{{ title }}
-          <b-icon icon="plus" class="plus" font-scale="1.5"></b-icon>
-          <b-icon icon="dash" class="minus" font-scale="1.5"></b-icon></b-button>
-      </b-card-header>
-      <b-collapse :id="id" accordion="faq-accordion" role="tabpanel">
-        <b-card-body>
-          <b-card-text><slot></slot></b-card-text>
-        </b-card-body>
-      </b-collapse>
-    </b-card>
+  <b-card no-body class="mb-1">
+    <b-card-header header-tag="header" class="p-1" role="tab">
+      <b-button block v-b-toggle="[id]" variant="neutral">{{ title }}
+        <b-icon icon="plus" class="plus" font-scale="1.5"></b-icon>
+        <b-icon icon="dash" class="minus" font-scale="1.5"></b-icon></b-button>
+    </b-card-header>
+    <b-collapse :id="id" accordion="faq-accordion" role="tabpanel">
+      <b-card-body>
+        <b-card-text><slot></slot></b-card-text>
+      </b-card-body>
+    </b-collapse>
+  </b-card>
 </template>
 
 <script type="text/javascript">
-  export default {
-    name: 'FaqEntry',
-    props: {
-        id: String,
-        title: String,
-    },
-  }
+export default {
+  name: 'FaqEntry',
+  props: {
+      id: String,
+      title: String,
+  },
+}
 </script>
 
 <style type="text/css" scoped>
   .card-header .btn {
-    text-align: left;
+    text-align: start;
   }
 
   .card-header .plus, .card-header .minus {
     position: absolute;
     top: .5em;
+  }
+
+  /* LTR specific styles */
+  :root[dir="ltr"] .card-header .plus,
+  :root[dir="ltr"] .card-header .minus {
     right: .3rem;
   }
+
+  /* RTL specific styles */
+  :root[dir="rtl"] .card-header .plus,
+  :root[dir="rtl"] .card-header .minus {
+    left: .3rem;
+  }
+
   .card-header .collapsed .minus {
     display: none;
   }
+
   .card-header .not-collapsed .plus {
     display: none;
   }
@@ -47,9 +60,11 @@
     margin-bottom: 0 !important;
     border-radius: 0;
   }
+
   .faq .card:not(:first-child) {
     border-top: 0;
   }
+
   .card-header {
     background-color: transparent;
     border: 0;
