284c7db46a9da39decce5f2130b7d1f87c993c61
Onyx2406 <yashsancheti24@gmail.com>
2024-04-11 15:16:22 +0530

    Fix - logo size was altered, now it remains constant

--- ui/src/components/NavBar.vue
+++ ui/src/components/NavBar.vue
@@ -1,18 +1,21 @@
 <template>
-  <header class="header">
-    <router-link :to="{ name: 'home' }" class="logo">
-      <img alt="$t('navbar.logoAlt')" src="../assets/ZIMIT_LOGO_RGB.svg">
-    </router-link>
-    <Loading class="loading"/>
-    
-    <!-- Language Dropdown -->
-    <select @change="changeLanguage($event)" class="language-selector">
-      <option value="en">English</option>
-      <option value="fr">Français</option>
-      <option value="fa">فارسی</option>
-      <!-- Add more languages here -->
-    </select>
-  </header>
+  <div class="nav-container">
+    <header class="header">
+      <router-link :to="{ name: 'home' }" class="logo">
+        <img alt="$t('navbar.logoAlt')" src="../assets/ZIMIT_LOGO_RGB.svg">
+      </router-link>
+      <Loading class="loading"/>
+    </header>
+
+    <div class="language-selector-container">
+      <select @change="changeLanguage($event)" class="language-selector">
+        <option value="en">English</option>
+        <option value="fr">Français</option>
+        <option value="fa">فارسی</option>
+        <!-- Add more languages here -->
+      </select>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -30,41 +33,41 @@ export default {
 </script>
 
 <style type="text/css" scoped="">
-  .header {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+  header {
+    display: inline-flex; 
+    justify-content: center;
+    width: 100%;
+    text-align: center;
     padding-top: 2em;
     padding-bottom: 2em;
-    text-align: center;
   }
 
-  .header .logo {
-    margin: 0 auto;
-  }
-
-  .header img {
-    max-width: 80%;
+  header img {
+    max-width: 50vw;
     width: 400px;
+  }
+
+  .language-selector-container {
+    position: absolute;
+    top: 2em;
+    right: 2em;
+    margin-left: auto; 
   }
 
   .language-selector {
     padding: .5rem 1rem;
     border-radius: .25rem;
     border: 1px solid #ced4da;
-    appearance: none;
+    appearance: none; 
     background-color: #fff;
-    margin-right: 1em; 
-    background-image: url('data:image/svg+xml;charset=UTF8,<svg ...></svg>');
+    background-image: url('data:image/svg+xml;charset=UTF8,<svg ...></svg>'); /* Add a custom dropdown arrow */
     background-repeat: no-repeat;
     background-position: right .75rem center;
     background-size: 16px 12px;
   }
-  
-  /* Add RTL support */
-  [dir="rtl"] .language-selector {
-    margin-right: 0;
-    margin-left: 1em;
-    background-position: left .75rem center;
+
+  [dir='rtl'] .language-selector-container {
+    right: auto;
+    left: 2em;
   }
 </style>
