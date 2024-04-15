3c8c7a59872917554a4a10d62f9d1aacc0ef09a1
benoit74 <benoit74@users.noreply.github.com>
2024-04-15 14:37:50 +0000

    Add icons

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -2,6 +2,18 @@ import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
 
+const app = createApp(App);
+
+// Font-Awesome
+import { aliases, fa } from 'vuetify/iconsets/fa-svg';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+app.component('font-awesome-icon', FontAwesomeIcon); // Register component globally
+library.add(fas); // Include needed solid icons
+library.add(far); // Include needed regular icons
+
 // Vuetify
 import 'vuetify/styles';
 import { createVuetify } from 'vuetify';
@@ -11,9 +23,16 @@ import * as directives from 'vuetify/directives';
 const vuetify = createVuetify({
   components,
   directives,
+  icons: {
+    defaultSet: 'fa',
+    aliases,
+    sets: {
+      fa,
+    },
+  },
 });
 
-createApp(App).use(vuetify).mount('#app');
+app.use(vuetify).mount('#app');
 
 //console.log(import.meta.env.VITE_APP_TITLE)
 // console.log(import.meta.env)
