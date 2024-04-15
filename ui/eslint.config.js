87f1a7cd451e4aa972c017a30d68ba9af2e77bf1
benoit74 <benoit74@users.noreply.github.com>
2024-04-15 12:45:22 +0000

    Add prettier and eslint

---
+++ ui/eslint.config.js
@@ -0,0 +1,13 @@
+import pluginVue from 'eslint-plugin-vue';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default [
+  ...pluginVue.configs['flat/recommended'],
+  eslintConfigPrettier,
+  {
+    rules: {
+      semi: 'error',
+      'prefer-const': 'error',
+    },
+  },
+];
