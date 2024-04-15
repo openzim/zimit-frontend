ac666d2d23c54f8acf902e1f196f1432c1ac0b8c
benoit74 <benoit74@users.noreply.github.com>
2024-04-15 13:58:51 +0000

    Add Vuetify

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -9,6 +9,19 @@ import HelloWorld from './components/HelloWorld.vue';
     </a>
   </div>
   <HelloWorld msg="Are you Vite + Vue" />
+  <div>
+    <div class="text-subtitle-2 mt-4 mb-2">Accordion</div>
+
+    <v-expansion-panels variant="accordion">
+      <v-expansion-panel
+        v-for="i in 3"
+        :key="i"
+        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+        title="Item"
+      ></v-expansion-panel>
+    </v-expansion-panels>
+    ¨
+  </div>
 </template>
 
 <style scoped>
@@ -34,5 +47,9 @@ footer {
 [dir='rtl'] footer {
   direction: rtl;
   text-align: center; /* Keep footer text centered or change as needed */
+}
+
+.logo {
+  width: 400px;
 }
 </style>
--- ui/src/main.ts
+++ ui/src/main.ts
@@ -1,8 +1,19 @@
-import { createApp } from "vue";
-import "./style.css";
-import App from "./App.vue";
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
 
-createApp(App).mount("#app");
+// Vuetify
+import 'vuetify/styles';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
+createApp(App).use(vuetify).mount('#app');
 
 //console.log(import.meta.env.VITE_APP_TITLE)
 // console.log(import.meta.env)
