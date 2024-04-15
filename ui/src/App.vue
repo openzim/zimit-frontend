64969656c609cd924bc5896f548f105ad33e3327
benoit74 <benoit74@users.noreply.github.com>
2024-04-15 13:10:28 +0000

    Fix

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
+import HelloWorld from './components/HelloWorld.vue';
 </script>
 
 <template>
@@ -8,7 +8,7 @@ import HelloWorld from "./components/HelloWorld.vue";
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld msg="Are you Vite + Vue" />
 </template>
 
 <style scoped>
